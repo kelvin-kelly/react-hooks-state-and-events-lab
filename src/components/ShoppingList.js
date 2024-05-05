439d8911fcf798317551e6b38e8d15413edb7793
Kelvin Machira <machirakelvin81@gmail.com>
2024-05-05 14:54:51 +0300

    use state and event handelling

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
-import React from "react";
+
+import React, { useState } from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+const ShoppingList = ({ items }) => {
+  const [selectCategory, setSelectCategory] = useState("All");
+
+  const handleChange = (e) => {
+    setSelectCategory(e.target.value);
+  };
+
+  const filteredItems =
+    selectCategory === "All"
+      ? items
+      : items.filter((item) => item.category === selectCategory);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +25,12 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default ShoppingList;
